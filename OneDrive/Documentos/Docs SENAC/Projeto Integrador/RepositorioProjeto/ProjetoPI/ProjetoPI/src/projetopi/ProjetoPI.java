4f2ed73b14670be888fbbafc7e12c80ef08c7b7c
VictorNess <vhps800@gmail.com>
2021-10-24 11:48:09 -0200

    Commit inicial

---
+++ OneDrive/Documentos/Docs SENAC/Projeto Integrador/RepositorioProjeto/ProjetoPI/ProjetoPI/src/projetopi/ProjetoPI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetopi;
+
+/**
+ *
+ * @author Victo
+ */
+public class ProjetoPI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Utilizando o Git atraves do Netbeans");
+    }
+    
+}
